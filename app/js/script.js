be54917990d1664a72f097575a3987ba3665a6fb
AnaLuizaBarros <analuizagbarros@gmail.com>
2021-08-15 11:24:30 -0300

    Final project

--- app/js/script.js
+++ app/js/script.js
@@ -1,32 +1,3 @@
-var $top = 215;
-var $Lastscroll = 0;
-$(document).on("scroll", function () {
-  console.clear();
-  console.log($(this).scrollTop());
-  $Actualscroll = $(this).scrollTop();
-  if ($Actualscroll > $Lastscroll) {
-    $top += 3;
-  } else if ($Actualscroll === 0) {
-    $top = 215;
-  } else if ($Actualscroll === 1564) {
-    $top = 430;
-    console.log("oi");
-  } else {
-    $top -= 3;
-  }
-  $("#make-contact").css("top", $top + "px");
-
-  $Lastscroll = $Actualscroll;
-  console.log($top);
-  console.log($Actualscroll);
-});
-function next() {
-  $("#carCarousel").slick("slickNext");
-}
-
-function prev() {
-  $("#carCarousel").slick("slickPrev");
-}
 $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
